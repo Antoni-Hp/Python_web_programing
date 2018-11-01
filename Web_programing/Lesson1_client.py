7b0262af690bf38eddd5ff2e78b8d6e8accb1abc
Henryk Panas <henrykantoni@hotmail.com>
2018-11-01 11:10:36 +0100

    client

--- Web_programing/Lesson1_client.py
+++ Web_programing/Lesson1_client.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
 import socket
-con_adres = ('0.0.0.0', 6500)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.connect(con_adres)
-sock.sendall("du")
-conn.close()
+def message():
+  message = raw_input("Give command 'key_in -get { key_number }' or 'key_in -set { key_number }': ")
+  return message
+
+class client():
+
+  def __init__(self):
+    self.adres = ('127.0.0.1', 6500)
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+  def connection(self, message):
+    self.sock.connect(self.adres)
+    self.sock.sendall(str(message))
+    data = self.sock.recv(16)
+    print data
+    self.connection_close()
+
+  def connection_close(self):
+    print "cos"
+    self.sock.close()
+
+try:
+  start = client()
+  start.connection(message())
+except:
+  raise
+  print 'error'
